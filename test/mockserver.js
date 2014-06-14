d59ffa33695a44a0c9fa3f77f8d9b507bdf07625
Ke Zhu <davidzhu.4.2@gmail.com>
2014-06-14 09:46:47 -0400

    test relative url after redirct

--- test/mockserver.js
+++ test/mockserver.js
@@ -41,7 +41,9 @@ app.use("/mockfiles/gzipped/",express.compress());
 
 app.use('/mockfiles/', express["static"](path.resolve(__dirname, 'mockfiles')));
 
-
+app.get('/redirect/links2.html', function(req, res){
+  res.redirect('/mockfiles/links2.html', 301);
+});
 
 
 exports.app = app;
