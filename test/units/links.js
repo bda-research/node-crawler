038fc883b7b8484c28e26b96cdf674f4735e3771
Ke Zhu <davidzhu.4.2@gmail.com>
2014-07-04 09:11:07 +0800

    support redirect by using correct document URI

--- test/units/links.js
+++ test/units/links.js
@@ -35,3 +35,32 @@ test('links resolve to absolute urls', function() {
         }
     }]);
 });
+
+test("relative links resolve to absolute urls after redirect", function() {
+    expect( 1 );
+
+    stop();
+
+    var c = new Crawler({
+        "debug":DEBUG,
+        "timeout":500,
+        "retryTimeout":1000,
+        "retries":1,
+        "onDrain":function() {
+            start();
+        }
+    });
+
+    c.queue([{
+        "uri":"http://127.0.0.1:"+MOCKPORT+"/redirect/links2.html",
+        "callback":function(error,result,$) {
+
+            var links = _.map($("a"),function(a) {
+                return a.href;
+            });
+
+            // relative link should be resolve to correct absolute URL
+            equal(links[0],"http://127.0.0.1:"+MOCKPORT+"/mockfiles/links1.html");
+        }
+    }]);
+});
