f869cadba59132ffedb6c81aadd9471f54630535
Paul Valla <paul.valla@gmail.com>
2014-11-07 16:57:56 +1100

    Update jquery tests

--- test/jquery.test.js
+++ test/jquery.test.js
@@ -9,93 +9,124 @@ describe('Jquery testing', function() {
     afterEach(function() {
         c = {};
     });
-    given.async('jsdom', 'cheerio')
-        .it('should work on inline html', function(done, jquery) {
-        c = new Crawler();
-        c.queue([{
-            html: '<p><i>great!</i></p>',
-            jquery: jquery,
-            callback: function(error, result, $) {
-                expect(error).to.be.null;
-                expect($('i').html()).to.equal('great!');
-                done();
-            }
-        }]);
+    describe('Jquery parsing', function() {
+        given.async('jsdom', 'cheerio')
+            .it('should work on inline html', function(done, jquery) {
+                c = new Crawler();
+                c.queue([{
+                    html: '<p><i>great!</i></p>',
+                    jquery: jquery,
+                    callback: function(error, result, $) {
+                        expect(error).to.be.null;
+                        expect($('i').html()).to.equal('great!');
+                        done();
+                    }
+                }]);
+            });
     });
-    it('should enable jQuery by default', function(done) {
-        c = new Crawler({
-            callback:function(error, result, $) {
-                expect(error).to.be.null;
-                expect($).not.to.be.null;
-                done();
-            }
+    describe('Jquery injection', function() {
+        it('should enable jsdom by default', function(done) {
+            c = new Crawler({
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($.fn.jquery).to.equal('1.8.3');
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/']);
         });
-        c.queue(['http://'+httpbinHost+'/']);
+        given.async('jsdom', {name: 'jsdom'}).it('should enable jsdom if set', function(done, jquery) {
+            c = new Crawler({
+                jquery: jquery,
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($.fn.jquery).to.equal('1.8.3');
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/']);
+        });
+        given.async('cheerio', {name: 'cheerio'}).it('should enable cheerio if set', function(done, jquery) {
+            c = new Crawler({
+                jquery: jquery,
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($.fn).to.be.undefined;
+                    expect(typeof $.root).to.equal('function');
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/']);
+        });
+        it('should disable jQuery if set to false', function(done) {
+            c = new Crawler({
+                jQuery: false,
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($).to.be.undefined;
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/']);
+        });
+        given.async('trucmuch', null, undefined).it('should not inject jquery', function(done, jquery) {
+            c = new Crawler({
+                jquery: jquery,
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($).to.be.undefined;
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/']);
+        });
+        it('should auto-disabling of jQuery if no html tag first', function(done) {
+            c = new Crawler({
+                jQuery: true,
+                callback:function(error, result, $) {
+                    expect(error).to.be.null;
+                    expect($).to.be.undefined;
+                    done();
+                }
+            });
+            c.queue(['http://'+httpbinHost+'/status/200']);
+        });
+        it('should work if jquery is set instead of jQuery when building Crawler', function(done) {
+            c = new Crawler({
+                maxConnections: 10,
+                jquery: true,
+                onDrain: function() {
+                    done();
+                },
+                callback: function(error, result, $) {
+                    expect($).not.to.be.undefined;
+                    expect(result.options.jQuery).to.be.true;
+                    expect(result.options.jquery).to.be.undefined;
+                }
+            });
+            c.queue(['http://'+httpbinHost]);
+        });
+        it('should work if jquery is set instead of jQuery when queuing', function(done) {
+            c = new Crawler({
+                maxConnections: 10,
+                jQuery: true,
+                onDrain: function() {
+                    done();
+                },
+                callback: function(error, result, $) {
+                    expect($).to.be.undefined;
+                    expect(result.options.jQuery).to.be.false;
+                }
+            });
+            c.queue([
+                {
+                    uri: 'http://'+httpbinHost,
+                    jquery : false
+                }
+            ]);
+        });
     });
-    it('should enable jsdom by default', function(done) {
-        c = new Crawler({
-            callback:function(error, result, $) {
-                expect(error).to.be.null;
-                expect($.fn.jquery).to.equal('1.8.3');
-                done();
-            }
-        });
-        c.queue(['http://'+httpbinHost+'/']);
-    });
-    it('should disable jQuery', function(done) {
-        c = new Crawler({
-            jQuery: false,
-            callback:function(error, result, $) {
-                expect(error).to.be.null;
-                expect($).to.be.undefined;
-                done();
-            }
-        });
-        c.queue(['http://'+httpbinHost+'/']);
-    });
-    it('should auto-disabling of jQuery if no html tag first', function(done) {
-        c = new Crawler({
-            jQuery: true,
-            callback:function(error, result, $) {
-                expect(error).to.be.null;
-                expect($).to.be.undefined;
-                done();
-            }
-        });
-        c.queue(['http://'+httpbinHost+'/status/200']);
-    });
-    it('should work if jquery is set instead of jQuery when building Crawler', function(done) {
-        c = new Crawler({
-            maxConnections: 10,
-            jquery: true,
-            onDrain: function() {
-                done();
-            },
-            callback: function(error, result, $) {
-                expect($).not.to.be.undefined;
-                expect(result.options.jQuery).to.be.true;
-                expect(result.options.jquery).to.be.undefined;
-            }
-        });
-        c.queue(['http://'+httpbinHost]);
-    });
-    it('should work if jquery is set instead of jQuery when queuing', function(done) {
-        c = new Crawler({
-            maxConnections: 10,
-            jQuery: true,
-            onDrain: function() {
-                done();
-            },
-            callback: function(error, result, $) {
-                expect($).to.be.undefined;
-                expect(result.options.jQuery).to.be.false;
-            }
-        });
-        c.queue([
-            {
-                uri: 'http://'+httpbinHost,
-                jquery : false
-            }
-        ]);
+    describe('Cheerio specific test', function() {
+
     });
 });
