2f977655fc3ba99da88f0efeddc92a8c5c74ba60
Paul Valla <paul.valla@gmail.com>
2014-11-07 19:03:19 +1100

    Remove dead code Fix a bug where onDrain wasnt called Rename functions to avoid ambiguity about CrawlerRequest and HttpRequest Moved a good chunk of code to a buildHttpRequest function
    Installed sinon
    Created cache test skeleton

--- tests/cache.test.js
+++ tests/cache.test.js
@@ -1,5 +1,6 @@
 var Crawler = require('../lib/crawler');
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var httpbinHost = 'localhost:8000';
 var c;
 
@@ -8,32 +9,21 @@ describe('Cache features tests', function() {
         afterEach(function () {
             c = {};
         });
-        it('should crawl one url', function (done) {
+        it.skip('should crawl one url', function (done) {
             c = new Crawler({
                 maxConnections: 1,
+                cache: true,
                 jquery: false,
                 onDrain: function () {
+                    expect(spy.calledOnce).to.be.true;
                     done();
                 },
-                callback: function (error, result, $) {
+                callback: function (error, result) {
                     expect(error).to.be.null;
                     expect(result.statusCode).to.equal(200);
                 }
             });
-            c.queue(['http://' + httpbinHost, 'http://' + httpbinHost, 'http://' + httpbinHost, 'http://' + httpbinHost]);
-        });
-        it('should crawl all url', function (done) {
-            c = new Crawler({
-                maxConnections: 1,
-                jquery: false,
-                onDrain: function () {
-                    done();
-                },
-                callback: function (error, result, $) {
-                    expect(error).to.be.null;
-                    expect(result.statusCode).to.equal(200);
-                }
-            });
+            var spy = sinon.spy(c, '_buildHttpRequest');
             c.queue(['http://' + httpbinHost, 'http://' + httpbinHost, 'http://' + httpbinHost, 'http://' + httpbinHost]);
         });
     });
