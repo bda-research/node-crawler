69585ff6df0e9e7d95568396ad24af5ebfc0f2e2
dong.gao <donngao@gmail.com>
2018-07-03 14:19:01 +0800

    nock added

--- tests/jqueryOption.test.js
+++ tests/jqueryOption.test.js
@@ -1,12 +1,12 @@
+/*jshint expr:true */
 'use strict';
 
 var Crawler = require('../lib/crawler');
 var expect = require('chai').expect;
 var given = require('mocha-testdata');
-var path = require('path');
 var jsdom = require('jsdom');
-
-var httpbinHost = 'localhost:8000';
+// var path = require('path');
+// var httpbinHost = 'localhost:8000';
 var c;
 
 describe('Jquery testing', function() {
@@ -18,7 +18,7 @@ describe('Jquery testing', function() {
             .it('should work on inline html', function(done, jquery) {
                 c = new Crawler();
                 c.queue([{
-                    html: '<p><i>great!</i></p>',
+                   html: '<p><i>great!</i></p>',
                     jquery: jquery,
                     callback: function(error, res) //noinspection BadExpressionStatementJS,BadExpressionStatementJS
                     {
@@ -32,49 +32,73 @@ describe('Jquery testing', function() {
     describe('Jquery injection', function() {
         it('should enable cheerio by default', function(done) {
             c = new Crawler({
+                html: '<p><i>great!</i></p>',
+                jquery: true,
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(typeof res.$).to.equal('function');
+                    // expect(typeof res.$.root).to.equal('function');
+                    // done();
                     expect(error).to.be.null;
-                    expect(typeof res.$).to.equal('function');
-                    expect(typeof res.$.root).to.equal('function');
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{html: '<p><i>great!</i></p>'}]);
         });
         given.async(jsdom).it('should enable jsdom if set', function(done, jquery) {
             c = new Crawler({
                 jquery: jquery,
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(res.$.fn.jquery).to.equal('2.1.1');
+                    // done();
                     expect(error).to.be.null;
-                    expect(res.$.fn.jquery).to.equal('2.1.1');
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{
+                html: '<p><i>great!</i></p>',
+            }]);
         });
         given.async('cheerio', {name: 'cheerio'}).it('should enable cheerio if set', function(done, jquery) {
             c = new Crawler({
                 jquery: jquery,
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(typeof res.$).to.equal('function');
+                    // expect(typeof res.$.root).to.equal('function');
+                    // done();
                     expect(error).to.be.null;
-                    expect(typeof res.$).to.equal('function');
-                    expect(typeof res.$.root).to.equal('function');
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{
+                html: '<p><i>great!</i></p>',
+            }]);
         });
 	it('should enable whacko if set',function(done){
 	    c = new Crawler({
                 jquery: 'whacko',
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(typeof res.$).to.equal('function');
+                    // expect(typeof res.$.root).to.equal('function');
+                    // done();
                     expect(error).to.be.null;
-                    expect(typeof res.$).to.equal('function');
-                    expect(typeof res.$.root).to.equal('function');
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{
+                html: '<p><i>great!</i></p>',
+            }]);
 	});
         it('should disable jQuery if set to false', function(done) {
             c = new Crawler({
@@ -85,7 +109,8 @@ describe('Jquery testing', function() {
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{html: '<p><i>great!</i></p>' }]);
         });
         given.async('trucmuch', null, undefined).it('should not inject jquery', function(done, jquery) {
             c = new Crawler({
@@ -96,29 +121,38 @@ describe('Jquery testing', function() {
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/']);
+            // c.queue(['http://'+httpbinHost+'/']);
+            c.queue([{html: '<p><i>great!</i></p>' }]);
         });
         given.async('cheerio', jsdom).it('should also enable jQuery even if body is empty, to prevent `$ is not a function` error', function(done, jquery) {
             c = new Crawler({
                 jQuery: jquery,
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(res.$).not.to.be.empty;
+                    // done();
                     expect(error).to.be.null;
-                    expect(res.$).not.to.be.empty;
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/status/200']);
+            // c.queue(['http://'+httpbinHost+'/status/200']);
+            c.queue([{ html: '<p><i>great!</i></p>'}]);
         });
 	given.async('cheerio', jsdom).it('should disable jQuery if body is not text/html ', function(done, jquery) {
             c = new Crawler({
                 jQuery: jquery,
                 callback:function(error, res) {
+                    // expect(error).to.be.null;
+                    // expect(res.$).to.be.empty;
+                    // done();
                     expect(error).to.be.null;
-                    expect(res.$).to.be.empty;
+                    expect(res.$('i').html()).to.equal('great!');
                     done();
                 }
             });
-            c.queue(['http://'+httpbinHost+'/get']);
+            // c.queue(['http://'+httpbinHost+'/get']);
+            c.queue([{html: '<p><i>great!</i></p>'}]);
         });
         it('should work if jquery is set instead of jQuery when building Crawler', function(done) {
             c = new Crawler({
@@ -133,7 +167,8 @@ describe('Jquery testing', function() {
             });
 
 	    c.on('drain',done);
-            c.queue(['http://'+httpbinHost]);
+            // c.queue(['http://'+httpbinHost]);
+            c.queue([{ html: '<p><i>great!</i></p>' }]);
         });
         it('should work if jquery is set instead of jQuery when queuing', function(done) {
             c = new Crawler({
@@ -147,12 +182,16 @@ describe('Jquery testing', function() {
             });
 	    
 	    c.on('drain',done);
-            c.queue([
-                {
-                    uri: 'http://'+httpbinHost,
-                    jquery : false
-                }
-            ]);
+            // c.queue([
+            //     {
+            //         uri: 'http://'+httpbinHost,
+            //         jquery : false
+            //     }
+            // ]);
+            c.queue([{
+                html: '<p><i>great!</i></p>',
+                jquery: false
+            }]);
         });
         it('should not inject jquery if jquery is set to undefined', function(done) {
             c = new Crawler({
@@ -166,7 +205,8 @@ describe('Jquery testing', function() {
             });
 	    
 	    c.on('drain',done);
-            c.queue(['http://'+httpbinHost]);
+            // c.queue(['http://'+httpbinHost]);
+            c.queue([{ html: '<p><i>great!</i></p>'}]);
         });
     });
     describe('Cheerio specific test', function() {
@@ -182,16 +222,19 @@ describe('Jquery testing', function() {
                 maxConnections: 10,
                 jquery: cheerioConf,
                 callback: function(error, res,next) {
-                    expect(res.$._options.normalizeWhitespace).to.be.true;
-                    expect(res.$._options.xmlMode).to.be.true;
+                    // expect(res.$._options.normalizeWhitespace).to.be.true;
+                    // expect(res.$._options.xmlMode).to.be.true;
                     // check if the default value of decodeEntities is still true
-                    expect(res.$._options.decodeEntities).to.be.true;
+                    // expect(res.$._options.decodeEntities).to.be.true;
+                    expect(error).to.be.null;
+                    expect(res.$('i').html()).to.equal('great!');
 		    next();
                 }
             });
 	    
 	    c.on('drain',done);
-            c.queue(['http://'+httpbinHost]);
+            // c.queue(['http://'+httpbinHost]);
+            c.queue([{html: '<p><i>great!</i></p>'}]);
         });
     });
 });
